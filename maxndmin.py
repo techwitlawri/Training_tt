ced00ea5763ee0289d4893cc5ff660f241730f15
techwitlawri <mmachilawri@gmail.com>
2023-07-06 21:33:05 +0100

    maxi

---
+++ maxndmin.py
@@ -0,0 +1,21 @@
+if __name__=='__main__':
+    numbers = []
+    maximum = 0
+    minimum = 0
+
+
+    for n in range(10):
+        number = int(input('Enter Number: '))
+        numbers.append(number)
+
+    for j in range(
+        
+        len(numbers)):
+        if numbers[j] > numbers[+1]:
+            maximum = numbers[j] 
+
+
+    print(numbers)
+    print(maximum)   
+
+    
