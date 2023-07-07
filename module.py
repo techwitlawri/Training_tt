aa2644a0c9630cd9489649f193be5d1e026c0900
techwitlawri <mmachilawri@gmail.com>
2023-07-07 15:32:19 +0100

    learning p2

---
+++ module.py
@@ -0,0 +1,10 @@
+import os 
+if __name__== '__main__':
+    num1 = 3
+    num2 = 140
+    while num1< num2:
+        print(num1)
+        num1+=1
+        if num1 == 120:
+            os.system('cls')
+            
