db596bc16e2ea408f4468ed13452c2f1b1720bfb
techwitlawri <mmachilawri@gmail.com>
2023-07-07 16:11:40 +0100

    math tt

--- math module.py
+++ math module.py
@@ -1,8 +1,11 @@
-from math import sqrt
+from math import sqrt, factorial
+import time 
 
 if __name__== '__main__':
-    square_root = sqrt(9)
-    print(square_root)
-
+  square_root = sqrt(9)
+  print(square_root)
+  time.sleep(3)
+  fac = factorial(10)
+  print(fac)
 
   
